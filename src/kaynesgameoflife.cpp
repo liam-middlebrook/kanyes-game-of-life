a8d073a51186a9d8a6cd63eccf9201d6c9630717
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-07-20 17:09:02 -0400

    Implemented rules of the game

--- src/kaynesgameoflife.cpp
+++ src/kaynesgameoflife.cpp
@@ -10,12 +10,19 @@ using namespace MINX::Graphics;
 
 bool* map;
 TextureBatch* texBatch;
-const int tileWidth = 32, tileHeight = 32;
+const int tileWidth = 16, tileHeight = 16;
+
+double elapsedTime = 0.0;
+double timeLimit = 0.5;
+
+
+bool* updateCells(bool* board, int width, int height);
+int calculateLiveNeighbors(bool* board, int x, int y, int width, int height);
 
 GameOfLife::GameOfLife() : Game()
 {
 	//This is the constructor. Put stuff here that should happen when the Game is created.
-
+	srand(time(NULL));
 	Game::SetVideoOptions(640, 480, false, "kaynes-game-of-life");
 }
 
@@ -57,6 +64,14 @@ void GameOfLife::UnloadContent()
 
 void GameOfLife::Update(GameTime * gameTime)
 {
+	elapsedTime += gameTime->GetDeltaTimeSeconds();
+
+	if(elapsedTime >= timeLimit)
+	{
+		elapsedTime = 0.0;
+
+		map = updateCells(map, GameWindow::GetWidth()/tileWidth, GameWindow::GetHeight()/tileHeight);
+	}
 	//Put stuff here to update the logic in your game each tick.
 	Game::Update(gameTime);
 }
@@ -72,7 +87,13 @@ void GameOfLife::Draw(GameTime * gameTime)
 	{
 		for(int x = 0; x < width; ++x)
 		{
-			texBatch->DrawPrimitiveRectangle(Rectangle(x * tileWidth, y * tileHeight, tileWidth, tileHeight), map[y * width + x] ? Color::Black : Color::Salmon);
+			texBatch->DrawPrimitiveRectangle(
+				Rectangle(
+					x * tileWidth,
+					y * tileHeight,
+					tileWidth,
+					tileHeight),
+				map[y * width + x] ? Color::Black : Color::Salmon);
 		}
 	}
 
@@ -80,4 +101,72 @@ void GameOfLife::Draw(GameTime * gameTime)
 
 	//Put stuff here to draw your game each frame.
 	Game::Draw(gameTime);
+}
+
+bool* updateCells(bool* board, int width, int height)
+{
+	bool* newBoard = (bool*)malloc(width * height);
+	for(int y = 0; y < height; ++y)
+	{
+		for(int x = 0; x < width; ++x)
+		{
+			int liveNeighbors = calculateLiveNeighbors(board, x, y, width, height);
+			if(board[y * width + x] && (liveNeighbors < 2) || liveNeighbors > 3)
+			{
+				newBoard[y * width + x] = false;
+			}
+			else if(board[y * width + x] && liveNeighbors < 4)
+			{
+				newBoard[y * width + x] = true;
+			}
+			else if(liveNeighbors == 3)
+			{
+				newBoard[y * width + x] = true;
+			}
+			else
+			{
+				newBoard[y * width + x] = false;
+			}
+		}
+	}
+	free(board);
+	return newBoard;
+}
+
+int calculateLiveNeighbors(bool* board, int x, int y, int width, int height)
+{
+	int liveCount = 0;
+	if(y > 0)
+	{
+		if(board[(y - 1) * width + x]) ++liveCount;
+	}
+	if(y > 0 && x > 0)
+	{
+		if(board[(y - 1) * width + (x - 1)]) ++liveCount;
+	}
+	if(x > 0)
+	{
+		if(board[y * width + (x - 1)]) ++liveCount;
+	}
+	if(x > 0 && y < height)
+	{
+		if(board[(y + 1) * width + (x - 1)]) ++liveCount;
+	}
+	if(y < height)
+	{
+		if(board[(y + 1) * width + x]) ++liveCount;
+	}
+	if(y < height && x < width)
+	{
+		if(board[(y + 1) * width + (x + 1)]) ++liveCount;
+	}
+	if(x < width)
+	{
+		if(board[y * width + (x + 1)]) ++liveCount;
+	}
+	if(y > 0 && x < width)
+	{
+		if(board[(y - 1) * width + (x + 1)]) ++liveCount;
+	}
+	return liveCount;
 }
